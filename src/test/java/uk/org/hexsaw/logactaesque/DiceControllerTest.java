edbac2312db1abfe29438e8677f324a1f1731d02
sawestwood <steve.westwood@hexsaw.org.uk>
2016-04-24 15:26:48 +0100

    DiceControllerTest introduced

---
+++ src/test/java/uk/org/hexsaw/logactaesque/DiceControllerTest.java
@@ -0,0 +1,74 @@
+package uk.org.hexsaw.logactaesque;
+
+import com.jayway.jsonpath.JsonPath;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiceConfiguration.class)
+@WebAppConfiguration
+public class DiceControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void canRollTheYellowDice() throws Exception {
+        this.mockMvc.perform(get("/dice/yellow/roll"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void rollingYellowDiceGivesZeroOneOrTwo() throws Exception {
+
+        ResultActions result = this.mockMvc.perform(get("/dice/yellow/roll"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+        String content =  result.andReturn().getResponse().getContentAsString();
+        Integer rolledNumber = JsonPath.read(content, "$.rolledNumber");
+        assertThat(rolledNumber, anyOf(is(0), is(1), is(2)));
+
+
+
+
+
+
+
+    }
+
+}
