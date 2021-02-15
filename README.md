8b2fae96fee458ff70e9c9a36133551914076c45
Steve Westwood <steve.westwood@hexsaw.org.uk>
2021-02-15 13:38:39 +0000

    Upgrade to Spring Boot 2.4.3, switch to YAML files

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 A RESTful web service built using [Spring Boot](https://spring.io/projects/spring-boot) that mimics the rolling of Logacta dice when determining the outcome of a football game.
 All seven dice are available as per the original rules of the game:
 
-| Dice | Available Faces  |
+| Dice  | Available Faces  |
 |-------|------------------|
 |Green  | 0, 1, 2, 3, 4, 5 |
 |Red    | 0, 1, 1, 2, 2, 3 |
@@ -19,20 +19,20 @@ To build and run the service locally via [Maven](https://maven.apache.org/):
 
     mvn spring-boot:run &
 
-This builds a jar file and then launches the basic service, by default accessible on port 8080.
+This builds a jar file and then launches the basic service, by default accessible on port 9001.
 ## Sample dice-rolling commands using curl
 A series of GET endpoints is included, each representing the roll of a specific Logacta dice:
 
-    curl localhost:8080/dice/green/roll
-    curl localhost:8080/dice/yellow/roll
-    curl localhost:8080/dice/brown/roll
-    curl localhost:8080/dice/red/roll
-    curl localhost:8080/dice/blue/roll
-    curl localhost:8080/dice/grey/roll
-    curl localhost:8080/dice/black/roll
+    curl localhost:9001/dice/green/roll
+    curl localhost:9001/dice/yellow/roll
+    curl localhost:9001/dice/brown/roll
+    curl localhost:9001/dice/red/roll
+    curl localhost:9001/dice/blue/roll
+    curl localhost:9001/dice/grey/roll
+    curl localhost:9001/dice/black/roll
 
 ## Actuator-based commands using curl
 Because the implementation relies upon the [Spring Actuator](https://github.com/spring-projects/spring-boot/tree/master/spring-boot-project/spring-boot-actuator), the health endpoint has 
 been made visible to allow you to determine if the service is active and reachable.
 
-    curl localhost:8080/actuator/health
+    curl localhost:9001/actuator/health
--- src/main/java/uk/org/hexsaw/logactaesque/DiceController.java
+++ src/main/java/uk/org/hexsaw/logactaesque/DiceController.java
@@ -2,6 +2,7 @@ package uk.org.hexsaw.logactaesque;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,37 +33,37 @@ public class DiceController {
     @Autowired(required = true)
     private Rollable blackDice;
 
-    @RequestMapping(path = "/dice/yellow/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/yellow/roll")
     public @ResponseBody DiceRoll yellowDiceRoll() {
         return DiceRoll.invoke("Yellow", yellowDice.roll());
     }
 
-    @RequestMapping(path = "/dice/green/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/green/roll")
     public @ResponseBody DiceRoll greenDiceRoll() {
         return DiceRoll.invoke("Green", greenDice.roll());
     }
 
-    @RequestMapping(path = "/dice/brown/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/brown/roll")
     public @ResponseBody DiceRoll brownDiceRoll() {
         return DiceRoll.invoke("Brown", brownDice.roll());
     }
 
-    @RequestMapping(path = "/dice/blue/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/blue/roll")
     public @ResponseBody DiceRoll blueDiceRoll() {
         return DiceRoll.invoke("Blue", blueDice.roll());
     }
 
-    @RequestMapping(path = "/dice/grey/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/grey/roll")
     public @ResponseBody DiceRoll greyDiceRoll() {
         return DiceRoll.invoke("Grey", greyDice.roll());
     }
 
-    @RequestMapping(path = "/dice/red/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/red/roll")
     public @ResponseBody DiceRoll redDiceRoll() {
         return DiceRoll.invoke("Red", redDice.roll());
     }
 
-    @RequestMapping(path = "/dice/black/roll", method = RequestMethod.GET)
+    @GetMapping("/dice/black/roll")
     public @ResponseBody DiceRoll blackDiceRoll() {
         return DiceRoll.invoke("Black", blackDice.roll());
     }
