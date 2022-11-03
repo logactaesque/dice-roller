788db6782023654afa20a2d5da4c59c96bd1bf66
Steve Westwood <steve.westwood@hexsaw.org.uk>
2022-11-03 21:40:07 +0000

    Explicitly reference Java 17. Move to spring-boot 2.7.3 and remove dead references

--- src/main/java/uk/org/hexsaw/logactaesque/DiceController.java
+++ src/main/java/uk/org/hexsaw/logactaesque/DiceController.java
@@ -3,8 +3,6 @@ package uk.org.hexsaw.logactaesque;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uk.org.hexsaw.logactaesque.model.DiceRoll;
 import uk.org.hexsaw.logactaesque.model.Rollable;
